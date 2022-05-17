9abff4a69a408c2013984e40038092f9f32429d2
dplt1992 <aluno19169@ipt.pt>
2022-05-17 16:55:44 +0100

    Primeiro commit. Projeto vazio.

---
+++ FreeBooks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
