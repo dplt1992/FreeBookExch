be4643a5275cd88e247deafa9457b6e531f110ff
Flávio Oliveira - 21425 <62102961+Flavio-Oliveira-21425@users.noreply.github.com>
2022-05-31 15:33:36 +0100

    Select multiple files

---
+++ FreeBooks/Data/Migrations/20220531143014_testaaaa.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreeBooks.Data.Migrations
+{
+    public partial class testaaaa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- FreeBooks/Models/FotosView.cs
+++ FreeBooks/Models/FotosView.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -7,13 +6,10 @@ namespace FreeBooks.Models
 {
     public class FotosView
     {
-        /// <summary>
-        /// Foto
-        /// </summary>
-        //[Required]
+      
         [Display(Name = "Foto")]
         public string Foto { get; set; }
-        public string[] ListaFotos { get; set; }
+        public List<IFormFile> ListaFotos { get; set; }
     }
 }
 
