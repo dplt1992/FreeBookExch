66fd4e99f0920ef4a529dd073afaaf66af593d4c
dplt1992 <dplt1992@gmail.com>
2022-06-06 15:56:24 +0100

    Migração para inicializar a base de dados

---
+++ FreeBooks/Data/Migrations/20220606144632_InitDB.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreeBooks.Data.Migrations
+{
+    public partial class InitDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Utilizadores",
+                columns: table => new
+                {
+                    IdUser = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Foto = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoUtilizador = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utilizadores", x => x.IdUser);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Anuncios",
+                columns: table => new
+                {
+                    IdAnuncio = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Tipo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Estado = table.Column<bool>(type: "bit", nullable: false),
+                    DataLancamento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataExpiracao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UtilizadorFk = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Anuncios", x => x.IdAnuncio);
+                    table.ForeignKey(
+                        name: "FK_Anuncios_Utilizadores_UtilizadorFk",
+                        column: x => x.UtilizadorFk,
+                        principalTable: "Utilizadores",
+                        principalColumn: "IdUser",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ofertas",
+                columns: table => new
+                {
+                    IdOferta = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    AnuncioFk = table.Column<int>(type: "int", nullable: false),
+                    UtilizadorFk = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ofertas", x => x.IdOferta);
+                    table.ForeignKey(
+                        name: "FK_Ofertas_Anuncios_AnuncioFk",
+                        column: x => x.AnuncioFk,
+                        principalTable: "Anuncios",
+                        principalColumn: "IdAnuncio",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Ofertas_Utilizadores_UtilizadorFk",
+                        column: x => x.UtilizadorFk,
+                        principalTable: "Utilizadores",
+                        principalColumn: "IdUser",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    IdLivro = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Edicao = table.Column<int>(type: "int", nullable: false),
+                    Editora = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AnuncioFK = table.Column<int>(type: "int", nullable: true),
+                    OfertaFK = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.IdLivro);
+                    table.ForeignKey(
+                        name: "FK_Livros_Anuncios_AnuncioFK",
+                        column: x => x.AnuncioFK,
+                        principalTable: "Anuncios",
+                        principalColumn: "IdAnuncio");
+                    table.ForeignKey(
+                        name: "FK_Livros_Ofertas_OfertaFK",
+                        column: x => x.OfertaFK,
+                        principalTable: "Ofertas",
+                        principalColumn: "IdOferta");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacoes",
+                columns: table => new
+                {
+                    IdTransacao = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataTrasancao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AnuncioFK = table.Column<int>(type: "int", nullable: true),
+                    OfertaFk = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacoes", x => x.IdTransacao);
+                    table.ForeignKey(
+                        name: "FK_Transacoes_Anuncios_AnuncioFK",
+                        column: x => x.AnuncioFK,
+                        principalTable: "Anuncios",
+                        principalColumn: "IdAnuncio");
+                    table.ForeignKey(
+                        name: "FK_Transacoes_Ofertas_OfertaFk",
+                        column: x => x.OfertaFk,
+                        principalTable: "Ofertas",
+                        principalColumn: "IdOferta");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fotos",
+                columns: table => new
+                {
+                    IdFoto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Foto = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LivroFK = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fotos", x => x.IdFoto);
+                    table.ForeignKey(
+                        name: "FK_Fotos_Livros_LivroFK",
+                        column: x => x.LivroFK,
+                        principalTable: "Livros",
+                        principalColumn: "IdLivro",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Anuncios_UtilizadorFk",
+                table: "Anuncios",
+                column: "UtilizadorFk");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fotos_LivroFK",
+                table: "Fotos",
+                column: "LivroFK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_AnuncioFK",
+                table: "Livros",
+                column: "AnuncioFK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_OfertaFK",
+                table: "Livros",
+                column: "OfertaFK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ofertas_AnuncioFk",
+                table: "Ofertas",
+                column: "AnuncioFk");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ofertas_UtilizadorFk",
+                table: "Ofertas",
+                column: "UtilizadorFk");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_AnuncioFK",
+                table: "Transacoes",
+                column: "AnuncioFK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_OfertaFk",
+                table: "Transacoes",
+                column: "OfertaFk");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fotos");
+
+            migrationBuilder.DropTable(
+                name: "Transacoes");
+
+            migrationBuilder.DropTable(
+                name: "Livros");
+
+            migrationBuilder.DropTable(
+                name: "Ofertas");
+
+            migrationBuilder.DropTable(
+                name: "Anuncios");
+
+            migrationBuilder.DropTable(
+                name: "Utilizadores");
+        }
+    }
+}
